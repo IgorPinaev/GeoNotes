09511bbc6dce7e79d49940c6e6d896d5d558ce68
igorpinaev <pinaevigor01@gmail.com>
2019-05-21 13:27:56 +0500

    Initial Commit

---
+++ GeoNotes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GeoNotes
+//
+//  Created by Игорь Пинаев on 21/05/2019.
+//  Copyright © 2019 Igor Pinaev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
